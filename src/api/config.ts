dc62d6a55eade63baee28bf4efe3aee13e053b88
nkvch <nkvch.st@gmail.com>
2023-10-15 21:23:38 +0200

    url hoster

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,4 @@
 export default {
-  // baseUrl: 'http://localhost:5000',
-  baseUrl: 'https://k4eplw52uc.execute-api.eu-central-1.amazonaws.com/staging',
-  s3Url: 'https://berrmore-files-bucket.s3.eu-central-1.amazonaws.com',
+  baseUrl: "https://api.berrymore.by",
+  s3Url: "https://berrmore-files-bucket.s3.eu-central-1.amazonaws.com",
 };
